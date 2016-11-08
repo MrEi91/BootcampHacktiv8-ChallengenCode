5e3b4483ed6e109aa8fd5d699edc7202d146bed8
Eri Irawan <eryirawan91@gmail.com>
2016-11-09 01:37:13 +0700

    Task1.js

--- Week 2/Day 1/Task1.js
+++
@@ -1,3 +0,0 @@
-//Belajar Tentang Variabel pada JavaScript
-var Number1=33, Number2=6, string1='foo',string2='bar', boolean1=true, boolean2=false;
-console.log(Number1, Number2, string1, string2, boolean1, boolean2);
